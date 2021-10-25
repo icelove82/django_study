969a691d8aba183ba882220caf206fe428469ca0
YUN <yunmyeonghun@gmail.com>
2021-10-25 11:45:31 +0900

    study 03

--- meetups/views.py
+++ meetups/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello, World!')
+    # return HttpResponse('Hello, World!')
+    return render(request, 'meetups/index.html')
